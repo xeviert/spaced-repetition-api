04368e082e492679e281768a3e518e76e632eb2b
Xevier Turrubiartes <xevierturrubiartes@gmail.com>
2021-02-02 23:08:45 -0600

    first commit

---
+++ seeds/seed.tables.sql
@@ -0,0 +1,41 @@
+BEGIN;
+
+TRUNCATE
+  "word",
+  "language",
+  "user";
+
+INSERT INTO "user" ("id", "username", "name", "password")
+VALUES
+  (
+    1,
+    'admin',
+    'admin',
+    -- password = "pass"
+    '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
+  );
+
+INSERT INTO "language" ("id", "name", "user_id")
+VALUES
+  (1, 'Italian ', 1);
+
+INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
+VALUES
+  (1, 1, 'mangiamo', 'lets eat', 2),
+  (2, 1, 'insalata', 'salad', 3),
+  (3, 1, 'vino', 'wine', 4),
+  (4, 1, 'pesce', 'fish', 5),
+  (5, 1, 'pane', 'bread', 6),
+  (6, 1, 'caffè', 'an espresso', 7),
+  (7, 1, 'è delizioso', 'this is delicious', 8),
+  (8, 1, 'grazie', 'thank you', null);
+
+UPDATE "language" SET head = 1 WHERE id = 1;
+
+-- because we explicitly set the id fields
+-- update the sequencer for future automatic id setting
+SELECT setval('word_id_seq', (SELECT MAX(id) from "word"));
+SELECT setval('language_id_seq', (SELECT MAX(id) from "language"));
+SELECT setval('user_id_seq', (SELECT MAX(id) from "user"));
+
+COMMIT;
